b25f53d9f21be6f46eb19d5a7a1051584cb26433
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-10 21:39:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const arrayCounter = names.length;
+    const message = [];
+
+    for (let i = 0; i < arrayCounter; i++) {
+   message[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    }
+    return message;
+
+}
+function countDown() {
+    let countDown = 10;
+while (countDown >= 0) {
+    console.log(countDown--);
+}
+}
